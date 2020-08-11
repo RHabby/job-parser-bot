909ca70700440342df9a1a76874dc44a1d5bfc0e
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2020-08-11 11:58:58 +0300

    generate messages and send

---
+++ bot.py
@@ -0,0 +1,53 @@
+import telebot
+
+import config as c
+import parser
+
+bot = telebot.TeleBot(token=c.BOT_TOKEN)
+
+
+def generate_message(source: str) -> str:
+    if source == "habr":
+        html = parser.get_html(
+            url=c.HABR_BASE,
+            params=c.HABR_PARAMS
+        )
+        vacancies = parser.parse_habr_html(html=html)
+    elif source == "hh":
+        html = parser.get_html(
+            url=c.HH_BASE,
+            params=c.HH_PARAMS
+        )
+        vacancies = parser.parse_hh_html(html=html)
+
+    text = ""
+    for vacancy in vacancies:
+        skills = ', '.join(vacancy['skills']) if isinstance(
+            vacancy['skills'], list) else vacancy['skills'].replace(
+                '<highlighttext>', '').replace('</highlighttext>', '')
+
+        employment_type = ', '.join(vacancy['employment_type']) if isinstance(
+            vacancy['employment_type'], list) else vacancy['employment_type']
+
+        salary = vacancy['salary'] if vacancy['salary'] else "не указана"
+
+        text += f"[{vacancy['title']}]({vacancy['link']})\nЗП: {salary}\n\
+{skills}\n{', '.join(vacancy['meta'])}\n{employment_type}\n\n"
+
+    return text
+
+
+def send_jobs():
+    resources = ["hh", "habr"]
+    for source in resources:
+        text = generate_message(source=source)
+        bot.send_message(
+            chat_id=c.CHANNEL_ID,
+            text=f"*Источник вакансий: {source}*\n\n{text}",
+            parse_mode="markdown",
+            disable_web_page_preview=True
+        )
+
+
+if __name__ == "__main__":
+    pass
